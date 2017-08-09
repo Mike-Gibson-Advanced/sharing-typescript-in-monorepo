9dbacac9b0e2567d3f55e3ec9b6f3ce5032ae29d
Mike Gibson <mike_gibson@live.co.uk>
2017-08-09 17:15:20 +0100

    sym links work on mac - store in repo?

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,14 @@
 //import * as shared1 from "../../shared/src/shared";
- import * as shared2 from "shared/mySharedThing";
+ //import * as shared2 from "./shared/mySharedThing";
+ 
+// symlink
+import * as shared3 from "./shared/mySharedThing";
 
 export const result = {
     server: "server",
     //shared1: shared1.shared,
-    shared2: shared2.shared,
+    //shared2: shared2.shared,
+    shared3: shared3.shared,
 };
 
 console.log(result);
